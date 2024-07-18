2c8d79fcd04e4d90dbca9975fc80a166e3d67f03
ashup <ashupal0089@gmail.com>
2024-07-18 11:28:24 +0530

    Initial commit

---
+++ lib/counter/counter.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'counter_controller.dart';
+
+class CounterPage extends StatelessWidget {
+  final CounterController counterController = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetX Counter'),
+      ),
+      body: Center(
+        child: Obx(() => Text('Count: ${counterController.count}')),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: counterController.increment,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
